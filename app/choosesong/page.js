2cc8e6f094143a4e018f423f768be505496b7ced
KunakornChamgate <kunakorn.four@gmail.com>
2024-05-13 19:22:48 +0700

    edit Swiper

--- app/choosesong/page.js
+++ app/choosesong/page.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import SongCard from "@/components/SongCard";
 
+
 export default function ChooseSong() {
     return (
-        <main className="ml-10 mt-20 mr-10 relative">
-        <img src="back.svg" /> 
+        <main className="relative">
+
+        <section className="ml-10 mt-20 mr-10">
+            <img src="back.svg" /> 
             <section>
                 <div id="left text">
                     <h1 className="text-white text-3xl font-bold mt-5"> Hello, Kunakorn </h1>
@@ -14,29 +17,29 @@ export default function ChooseSong() {
                     <input type="text" placeholder="Search artist, song or album"  className="bg-neutral-600 px-12 py-3 w-full rounded-full" />
                 </div>
             </section>
-
             <section>
                 <div>
                     <h1 className="text-lime-300 text-xl font-regular pt-5"> Suggestion </h1>
                 </div>
             </section>
-
-            <section className="flex pt-3">
+            <section className="flex mt-5">
                 <SongCard imageSrc="/halftheworld.png" titleText="Half the world away" artistName="Oasis" size="large" />
             </section>
-            <section className="mt-">
-                <h1 className="text-lime-300 text-xl font-regular "> Recents </h1>
-                <div className="flex row pt-3 pr-10">
+        </section>
+
+            
+        <section>
+            <section className="ml-10 mt-20">
+                <h1 className="text-lime-300 text-xl font-regular">Recents</h1>
+                <div className="mt-3 flex flex-row">
                     <SongCard imageSrc="/stand.png" titleText="Stand by me" artistName="Oasis" size="small" />
-                    <SongCard imageSrc="/yellow.png" titleText="Yellow" artistName="Coldplay" size="small" />
                     <SongCard imageSrc="/all.png" titleText="Yellow" artistName="Coldplay" size="small" />
-                    {/* <SongCard imageSrc="/halftheworld.jpg" titleText="All my loving" artistName="The Beatles" size="small" />
-                    <SongCard imageSrc="/halftheworld.jpg" titleText="Golden" artistName="Harry Styles" size="small" />
-                    <SongCard imageSrc="/halftheworld.jpg" titleText="Massachusettes" artistName="Bee Gees" size="small" /> */}
+                    <SongCard imageSrc="/all.png" titleText="Song 3" artistName="Artist 3" size="small" />
+                    <SongCard imageSrc="/all.png" titleText="Song 3" artistName="Artist 3" size="small" />
                 </div>
             </section>
-
-            <section className="mt-5">
+``
+            <section className="mt-5 ml-10">
                 <h1 className="text-white text-xl font-regular"> Hits </h1>
                 <div className="flex row pt-3 pr-10">
                     <SongCard imageSrc="/halftheworld.jpg" titleText="Stand by me" artistName="Oasis" size="small" />
@@ -47,6 +50,7 @@ export default function ChooseSong() {
                     <SongCard imageSrc="/halftheworld.jpg" titleText="Massachusettes" artistName="Bee Gees" size="small" /> */}
                 </div>
             </section>
+        </section>
         </main>
     );
 }
